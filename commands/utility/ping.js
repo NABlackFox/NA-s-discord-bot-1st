1324296b7f127121cd36a138fc2dac4f55d59bfd
Kim Nhật Anh <thanhchem.pro@gmail.com>
2025-02-23 22:41:09 +0700

    adding utitily files for basic commad

---
+++ commands/utility/ping.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+
+    // Data will provide the information related to the commad
+	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with "Pong !"'),
+
+    // Logic of the commands
+	async execute(interaction) {
+		await interaction.reply('Pong !');
+	},
+};
